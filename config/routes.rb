c59ba99777a0673b2db7a307d735524a1636ed25
Natalia Harateh <n.harateh@gmail.com>
2017-03-03 11:24:39 +0100

    First

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # Because we want a listing of movies,
+  # by convention the name of the controller will be movies
+  # and the name of the action will be index.
+  get "movies" => "movies#index"
+end
---
+++ db/migrate/20170302013422_add_fields_to_movies.rb
@@ -0,0 +1,7 @@
+class AddFieldsToMovies < ActiveRecord::Migration[5.0]
+  def change
+    add_column :movies, :rating, :string
+    add_column :movies, :description, :text
+    add_column :movies, :released_on, :date
+  end
+end
