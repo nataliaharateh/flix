62fe69abee95c88efd03c4426bd891524ce4e625
Natalia Harateh <n.harateh@gmail.com>
2017-03-03 12:36:10 +0100

    add a route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # by convention the name of the controller will be movies
   # and the name of the action will be index.
   get "movies" => "movies#index"
+  get "movies/:id" => "movies#show"
 end
