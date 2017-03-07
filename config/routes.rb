4e08644dcac7be46608d3fca9a1ec0155953fca6
Natalia Harateh <n.harateh@gmail.com>
2017-03-07 22:41:23 +0000

    link pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Because we want a listing of movies,
   # by convention the name of the controller will be movies
   # and the name of the action will be index.
+  root "movies#index"
   get "movies" => "movies#index"
-  get "movies/:id" => "movies#show"
+  get "movies/:id" => "movies#show", as: "movie"
 end
