218b871545ae7c9c70fda07a535ae31e6269e9d4
Natalia Harateh <n.harateh@gmail.com>
2017-03-09 01:15:59 +0000

    add create forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   # by convention the name of the controller will be movies
   # and the name of the action will be index.
   root "movies#index"
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-  patch "movies/:id" => "movies#update"
+  resources :movies
+
 end
